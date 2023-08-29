e3ea84f7c159290c81ab1f65e14fd45d5a34048e
giulifarias05 <[gfarias@escuelasproa.edu.ar]>
2023-08-28 23:27:26 -0300

    agregando iconos y fondo

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,33 +10,41 @@ import Card from './components/cards'; // Importa el componente cards
 const App = () => {
   return (
     <div className="app-container">
-      <Navbar />
-      <main className="main-content">
-        <div className="cards-container">
-          <Card
-            imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/60003653fdb591dea610e9906601614b.webp?itok=r2LcWnO8"
-            title="Budín de chocolate con banana"
-            description="Receta para preparar un delicioso budín de chocolate con banana."
-          />
-          <Card
-            imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/b127f341d58fb406367f813a1eae2ee8.webp?itok=NVfT5zMt"
-            title="Tarta de Coco y Dulce de Leche"
-            description="Receta de tarta de coco y dulce de leche para cocinar en casa."
-          />
-          <Card
-            imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/efabc050b31d83ad98ca374b474f7775.webp?itok=sjW22IDF"
-            title="Bizcochuelo de vainilla casero"
-            description="Receta para preparar un delicioso bizcochuelo de vainilla"
-          />
-            <Card
-            imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/ae8634a783369f7e773a2a9ed67d4957.webp?itok=t7phrY1Q"
-            title="Torta Brownie"
-            description="Descubre como hacer esta deliciosa Torta Brownie."
-          />
-        </div>
-      </main>
-      <Footer />
+  <Navbar />
+  <main className="main-content">
+    <div className="cards-container">
+      {/* Primera fila */}
+      <div className="card-row">
+        <Card
+          imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/60003653fdb591dea610e9906601614b.webp?itok=r2LcWnO8"
+          title="Budín de chocolate con banana"
+          description="Receta para preparar un delicioso budín de chocolate con banana."
+        />
+        <Card
+          imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/b127f341d58fb406367f813a1eae2ee8.webp?itok=NVfT5zMt"
+          title="Tarta de Coco y Dulce de Leche"
+          description="Receta de tarta de coco y dulce de leche para cocinar en casa."
+        />
+      </div>
+
+      {/* Segunda fila */}
+      <div className="card-row">
+        <Card
+          imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/efabc050b31d83ad98ca374b474f7775.webp?itok=sjW22IDF"
+          title="Bizcochuelo de vainilla casero"
+          description="Receta para preparar un delicioso bizcochuelo de vainilla."
+        />
+        <Card
+          imageSrc="https://www.recetasnestle.com.ar/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/ae8634a783369f7e773a2a9ed67d4957.webp?itok=t7phrY1Q"
+          title="Torta Brownie paso a paso"
+          description="Descubre cómo hacer esta deliciosa Torta Brownie."
+        />
+      </div>
     </div>
+  </main>
+  <Footer />
+</div>
+
   );
 };
 
--- myapp/src/components/cards.js
+++ myapp/src/components/cards.js
@@ -5,21 +5,46 @@ const Card = (props) => {
 
   return (
     <div className="recipe-card">
-      <div className="card-img">
-        <img src={imageSrc} alt="Receta" />
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <div className="more-info-icon">
-          {/* Ícono de "+" sin fondo */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M12 2c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm2 11h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"/>
-          </svg>
+    <nav>
+      <ul className="card-content">
+        <li><a href="#"></a></li>
+        <div className="card-img">
+          <img src={imageSrc} alt="Receta" />
         </div>
-      </div>
-    </div>
+        <div className="card-info">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+        <div className="more-info-icons">
+  {/* Ícono de "+" sin fondo */}
+  <div className="icon">
+    
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12 2c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm2 11h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"/>
+    </svg>
+  </div>
+    <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12 2c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm2 11h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"/>
+    </svg>
+  </div>
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12 2c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm2 11h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"/>
+    </svg>
+  </div>
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12 2c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm2 11h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"/>
+    </svg>
+  </div>
+</div>
+      </ul>
+    </nav>
+  </div>
+  
   );
-};
-
+  }  
 export default Card;
+
+
