d57bffdcea8db1187c95533f8b29e86102345286
giulifarias05 <88504587+giulifarias05@users.noreply.github.com>
2023-09-20 13:09:27 -0300

    bajando iconos

--- myapp/src/components/cards.js
+++ myapp/src/components/cards.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Card = (props) => {
-  const { imageSrc, title, description, iconPath1,iconPath2,iconPath3,iconPath4, iconText1,iconText2,iconText3,iconText4} = props;
 
   return (
     <div className="recipe-card">
@@ -9,28 +8,28 @@ const Card = (props) => {
       <ul className="card-content">
         <li><a href="#"></a></li>
         <div className="card-img">
-          <img src={imageSrc} alt="Receta" />
+          <img src={props.imageSrc} alt="Receta" />
         </div>
         <div className="card-info">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
         </div>
         <div className="more-info-icons">
         <div className="icon">
-          <img src={iconPath1}  />
-          <span>{iconText1}</span>
+          <img src={props.iconPath1}  />
+          <span>{props.iconText1}</span>
         </div>
         <div className="icon">
-          <img src={iconPath2}  />
-          <span>{iconText2}</span>
+          <img src={props.iconPath2}  />
+          <span>{props.iconText2}</span>
         </div>
         <div className="icon">
-          <img src={iconPath3}  />
-          <span>{iconText3}</span>
+          <img src={props.iconPath3}  />
+          <span>{props.iconText3}</span>
         </div>
         <div className="icon">
-          <img src={iconPath4} />
-          <span>{iconText4}</span>
+          <img src={props.iconPath4} />
+          <span>{props.iconText4}</span>
         </div>
 </div>
       </ul>
